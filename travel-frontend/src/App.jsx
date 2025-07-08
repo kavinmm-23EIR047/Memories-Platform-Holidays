98566dd241c4ff90836a4ab4b4b4a4ffbcf7fa42
kavin <kavinmm.23eie@kongu.edu>
2025-07-08 20:00:06 +0530

    1

--- travel-frontend/src/App.jsx
+++ travel-frontend/src/App.jsx
@@ -1,32 +1,3 @@
-// import { Routes, Route } from "react-router-dom";
-// import Navbar from "./components/Navbar";
-// import Footer from "./components/Footer";
-// import MainScrollPage from "./components/MainScrollPage";
-// import GalleryBusPage from "./components/GalleryBusPage";
-// import Feedback from "./components/Feedback";
-// import Loader from "./components/Loader.jsx";
-// import { useState } from "react";
-// import "./index.css";
-
-// function App() {
-//   const [loading, setLoading] = useState(true);
-
-//   if (loading) return <Loader onComplete={() => setLoading(false)} />;
-
-//   return (
-//     <div className="bg-white text-gray-800">
-//       <Navbar />
-//       <Routes>
-//         <Route path="/" element={<MainScrollPage />} />
-//         <Route path="/gallery/:busId" element={<GalleryBusPage />} />
-//         <Route path="/feedback" element={<Feedback />} />
-//       </Routes>
-//       <Footer />
-//     </div>
-//   );
-// }
-
-// export default App;
 import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
